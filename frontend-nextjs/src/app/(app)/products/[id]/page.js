21475e35a5192b1660eb1b7ba08b11a9a72d8e37
Alexander <aleks.lipckin@gmail.com>
2024-09-13 17:12:28 +0500

    added new product rows

--- frontend-nextjs/src/app/(app)/products/[id]/page.js
+++ frontend-nextjs/src/app/(app)/products/[id]/page.js
@@ -44,25 +44,60 @@ export default function page({ params }) {
             <div className="container d-flex justify-content-start">
                 <div className="row mt-5 mb-5 d-flex justify-content-center col-12 col-sm-7">
                     <h4 className="mh">{data.name}</h4>
+                    <p>На {data.grm} грамм</p>
                     <div className="mt-4 d-flex flex-column justify-content-center">
+                    {data.fat !== null && data.fat !== undefined && (
                         <InfoCard title="Жиры" className="">
                             {data.fat} г.
                         </InfoCard>
+                    )}
+                    {data.protein !== null && data.protein !== undefined && (
                         <InfoCard title="Белки" className="">
                             {data.protein} г.
                         </InfoCard>
+                    )}
+                    {data.carbs !== null && data.carbs !== undefined && (
                         <InfoCard title="Углеводы" className="">
                             {data.carbs} г.
                         </InfoCard>
+                    )}
+                    {data.water !== null && data.water !== undefined && (
                         <InfoCard title="Вода" className="">
                             {data.water} г.
                         </InfoCard>
+                    )}
+                    {data.ash !== null && data.ash !== undefined && (
                         <InfoCard title="Зола" className="">
                             {data.ash} г.
                         </InfoCard>
+                    )}
+                    {data.fiber !== null && data.fiber !== undefined && (
                         <InfoCard title="Клетчатка" className="">
                             {data.fiber} г.
                         </InfoCard>
+                    )}
+                    {data.dry !== null && data.dry !== undefined && (
+                        <InfoCard title="Сухость" className="">
+                            {data.dry} % 
+                        </InfoCard>
+                    )}
+                    {data.alko !== null && data.alko !== undefined && (
+                        <InfoCard title="Алкоголь" className="">
+                            {data.alko} %
+                        </InfoCard>
+                    )}
+                    {data.sug !== null && data.sug !== undefined && (
+                        <InfoCard title="Сахароза" className="">
+                            {data.sug} %
+                        </InfoCard>
+                    )}
+                    {data.rem !== null && data.rem !== undefined && (
+                        <InfoCard title="Источник" className="">
+                            {data.rem} г.
+                        </InfoCard>
+                    )}
+
+
                     </div>
                 </div>
             </div>
--- frontend-nextjs/src/components/cards/InfoCard.js
+++ frontend-nextjs/src/components/cards/InfoCard.js
@@ -1,14 +1,19 @@
-import '@/components/Inputs/TtkInput.css'
-import "./InfoCard.css"
-export default function InfoCard({title, item, children, className, ...params}) {
+import '@/components/Inputs/TtkInput.css';
+import './InfoCard.css';
+
+export default function InfoCard({ title, item, children, className, ...params }) {
+    if (!children && !item) {
+        return null; // or you could return some other fallback UI if item is falsy
+    }
+
     return (
-            <div className={"d-flex justify-content-start row col-12 " + className}>
-                <div className="ttk-title col-3">
-                    {title}
-                </div>
-                <div className="input-box info-item text-end  col-9">
-                    {item}{children}
-                </div>
+        <div className={`d-flex justify-content-start row col-12 ${className}`}>
+            <div className="ttk-title col-3">
+                {title}
             </div>
-    )
+            <div className="input-box info-item text-end col-9">
+                {item}{children}
+            </div>
+        </div>
+    );
 }
